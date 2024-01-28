5bab65ae6556b4c19df2b4d46f268aa8f139deb2
mamania0217 <aryanmamania17@gmail.com>
2024-01-28 23:40:13 +0530

    made some small changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { HTML } from "@react-three/drei";
+
 
 const Loader = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'
-    </div>
-  )
-}
+    <html>
+      <div className="flex justify-center items-center">
+        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+      </div>
+     
+    </html>
 
-export default Loader
+  );
+};
+export default Loader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,6 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
 
-//<div className='absolute top-28 left-0 right-0 z-10 flex  items-center justify-center'>
-//POPUP
-///</div> 
-
 const Home = () => {
   return (
    <section className='w-full h-screen relative'>
@@ -14,7 +10,11 @@ const Home = () => {
 <Canvas className='w-full h-screen bg-transparent'>
   camera={{ near: 0.1, far:1000}}
   <Suspense fallback={<Loader />}>
-    
+    <directionalLight />
+    <ambientLight />
+    <pointLight />
+    <spotLight />
+    <hemisphereLight />
   </Suspense>
   </Canvas> 
    </section>
