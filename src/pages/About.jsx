85c71b371391321a996ee7351b1ed95a1e9dea85
mamania0217 <aryanmamania17@gmail.com>
2024-02-16 20:56:17 +0530

    done with contact use page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+   <section className='max-container'>
+    <h1 className='head-text'>
+      Hello, I am <span className='blue-gradient_text font-semibold drop-shadow'>Aryan</span>
+      </h1>
+      <div>
+        <p>Software Engineer</p>
+      </div>
+   </section>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,75 @@
-import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import React, { Suspense, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+
+import Loader from "../components/Loader";
+import Fox from "../models/fox";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
-  const [isLoading, setisLoading] = useState(false);
-  const handleChange = (e) => {
-setForm({...form, [e.target.name]: e.target.value })
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
 
+  const { alert, showAlert, hideAlert } = useAlert();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setisLoading(true)
+    setIsLoading(true);
+    setCurrentAnimation("run");
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Aryan",
+          to_email: form.email,
+          to_email: "aryanmamania02@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+
+        showAlert({
+          show: true,
+          text: "message sent successfully!",
+          type: "success",
+        });
+
+        setTimeout(() => {
+          hideAlert();
+          setCurrentAnimation("idle");
+          setForm({ name: "", email: "", message: "" });
+        }, [3000]);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setCurrentAnimation("idle");
+        console.log(error);
+        showAlert({
+          show: true,
+          text: "I didnt receive your message",
+          type: "danger",
+        });
+      });
   };
 
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
 
-
-  const handleFocus = () => {};
-  const handleBlur = () => {};
- 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in touch</h1>
         <form className="w-full flex flex-col gap-7 mt-14">
@@ -79,6 +127,28 @@ setForm({...form, [e.target.name]: e.target.value })
             {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
